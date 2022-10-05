20ed4ac2e605de65492e7af211f9e1713bd8fad7
deniandriancode <deniandriancode@gmail.com>
2022-10-05 12:57:13 +0700

    update

--- animelink.py
+++ animelink.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup as bsp
 import requests
 
-print("Python is here")
+with open("animelink.csv", "w") as fp:
+    fp.write("Dolor ut veritatis reprehenderit sed minus cupiditate? Placeat beatae dolorem tenetur quibusdam eaque velit Rem ex assumenda sunt repellendus error id? Ut deleniti itaque repellendus reprehenderit veritatis? Accusantium ad nesciunt")
